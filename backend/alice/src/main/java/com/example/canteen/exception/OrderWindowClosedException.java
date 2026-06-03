6bc19625038e11f8aa1c49258e103c65817e4d17
Alicyho <yanaliceho@gmail.com>
2026-06-03 14:54:33 +0800

    Delete backend/alice directory

--- backend/alice/src/main/java/com/example/canteen/exception/OrderWindowClosedException.java
+++
@@ -1,8 +0,0 @@
-package com.example.canteen.exception;
-
-public class OrderWindowClosedException extends RuntimeException {
-
-    public OrderWindowClosedException(String message) {
-        super(message);
-    }
-}
